beed742dd69c7a2b7e7c9432aa45cd873987b612
Alfian <alfianzzdwip@gmail.com>
2023-05-29 11:09:47 +0700

    Add Verification Feature

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -5,7 +5,7 @@ const productsService = new ProductsService();
 
 const cronJob = () => {
   //ekspresi jadwal 0 0 * * * berarti tugas akan dijalankan pada pukul 00:0 (0:00 PM) setiap hari.
-  cron.schedule("0 0 * * *", async () => {
+  cron.schedule("0 1 * * *", async () => {
     try {
       await productsService.deleteOldProducts();
       console.log("Old data deleted successfully.");
